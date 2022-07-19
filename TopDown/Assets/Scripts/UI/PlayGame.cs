5b855b00fef1a38d83ac97ad2993ab4892ab25d9
baotran6112000 <baotran6112000@gmail.com>
2022-07-19 18:19:51 +0700

    1

--- TopDown/Assets/Scripts/UI/PlayGame.cs
+++ TopDown/Assets/Scripts/UI/PlayGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayGame : MonoBehaviour
 {
@@ -31,5 +32,10 @@ public class PlayGame : MonoBehaviour
         buttonSettings.SetActive(true);
         button.SetActive(true);
         settingsMenu.SetActive(false);
+    }
+
+    public void Home(string name)
+    {
+        SceneManager.LoadScene(name);
     }
 }
